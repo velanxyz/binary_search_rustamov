5dd4cd86fb2847767f648fd50a0ac77d167da943
velanxyz <ravilrustamov00777@gmail.com>
2023-10-17 18:56:15 +0300

    некоторое не понял

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 #напишите программу для бинарного поиска загаданного числа
 
-def binary_search():
-    pass
+def binary_search(array, target):
+    low = 0
+    high = len(array) - 1
+    while low <= high:
+        mid = (low + high) // 2
+        if array[mid] == target:
+            return mid
+        elif array[mid] < target:
+            low = mid + 1
+        else:
+            high = mid - 1
+    return -1
